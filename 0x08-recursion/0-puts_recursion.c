f4c10db3e4cb7815f419f6910b91b3d469e0a035
3issam-hub <benissam723@gmail.com>
2023-04-14 17:43:20 -0700

    fix test

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+/**
+* _puts_recursion - function that prints a string using puts
+* @s : pointer to the string to be printed
+**/
 void  _puts_recursion(char *s)
 {
-    puts(s);
+puts(s);
 }

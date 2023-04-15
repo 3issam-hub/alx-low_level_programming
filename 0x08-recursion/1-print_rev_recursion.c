5b7f1dbb6855a9ae9a62a79e996b6423a44d2da7
3issam-hub <57049244+3issam-hub@users.noreply.github.com>
2023-04-15 13:54:41 +0000

    fixed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,11 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void _print_rev_recursion(char *s)
+void _print_rev_recursion (char *s)
 {
 if (*s == '\0')
 {
- return;
+return;
 }
 _print_rev_recursion (s + 1);
 _putchar (*s);
